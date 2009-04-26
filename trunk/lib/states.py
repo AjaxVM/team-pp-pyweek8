4eeb3c3f58f095b02870dee949c89ba70a4173be
RoeBros <RoeBros@1d227aee-313c-11de-8ef0-0f53c1b7e458>
2009-04-26 19:22:33 +0000

    Added button GUI. Made Button/Label take newlines in text...

--- trunk/lib/states.py
+++ trunk/lib/states.py
@@ -44,7 +44,10 @@ class Menu(GameState):
         GameState.__init__(self, parent)
 
         self.app = ui.App(self.get_root().screen)
-        ui.Label(self.app, "Testing, 123", text_color=(255,0,0), pos=(0,0), anchor="topleft")
+        ui.Label(self.app, "Testing,\n123", text_color=(255,0,0), pos=(0,0), anchor="topleft")
+
+        ui.Button(self.app, "click\nme!", text_color=(0,255,0), pos=(0,75),
+                  callback=lambda: ui.Label(self.app, "huh?", pos=(0,150)))
 
     def update(self):
         for event in pygame.event.get():
