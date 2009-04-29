316649d2d1c5d2c4a27daf578f88d8d3d31f86e4
RoeBros <RoeBros@1d227aee-313c-11de-8ef0-0f53c1b7e458>
2009-04-29 03:29:34 +0000

    Added pyggel in, setup basic menu control system thingy...

--- lib/main.py
+++ lib/main.py
@@ -1,5 +1,54 @@
+import pyggel
+from pyggel import *
 
+class Menu(object):
+    def __init__(self, state):
+        self.state = state
+        self.state.wave = 0 #reset this...
+
+        self.run()
+
+    def run(self):
+        print 32 #this should loop!
+        self.state.quit = True
+
+class AmmoRoom(object):
+    def __init__(self, state):
+        self.state = state
+        self.run()
+
+    def run(self):
+        print 33
+
+class FightScreen(object):
+    def __init__(self, state):
+        self.state = state
+        self.run()
+
+    def run(self):
+        print 34
+
+class GameController(object):
+    def __init__(self):
+        pyggel.init()
+
+        self.wave = 0
+
+        self.state = "menu"
+
+        self.quit = False
+
+    def run(self):
+        while not self.quit:
+            if self.state == "menu":
+                Menu(self)
+            if self.state == "ammo":
+                AmmoRoom(self)
+            if self.state == "game":
+                FightScreen(self)
+        pyggel.quit()
 
 def run():
-    print"running!"
-    raw_input()
+    a = GameController()
+    a.run()
+run()
