a9307b461a28abada6282cf7932e1f4a8dd58d15
nihilocrat <nihilocrat@1d227aee-313c-11de-8ef0-0f53c1b7e458>
2009-04-30 21:37:28 +0000

    added basic sound-playing code to fire 'pew-pew' noises when a tower fires.

---
+++ trunk/lib/sound.py
@@ -0,0 +1,20 @@
+import os
+import glob
+
+import pygame
+
+class SoundManager(object):
+    sounds = {}
+    music = {}
+    def __init__(_, basepath):
+        pygame.mixer.init()
+
+        # load all sounds found in data
+        for filename in glob.glob(os.path.join(basepath,'*.ogg')):
+            stripped_filename = filename.replace(os.path.join(basepath,''), '')
+            _.sounds[stripped_filename] = pygame.mixer.Sound(filename)
+
+        print "Loaded sounds:", `_.sounds.keys()`
+
+    def __del__(_):
+        pygame.mixer.quit()
